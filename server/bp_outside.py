d4ba4bf4a281edd9c6445e1f7b60e902e5833140
Steve Landey <steve@steveasleep.com>
2023-01-29 11:27:22 -0800

    Restore healt check and plan

--- server/bp_outside.py
+++ server/bp_outside.py
@@ -6,3 +6,8 @@ bp = Blueprint("outside", "outside", url_prefix="")
 @bp.route("/", methods=["GET"])
 def index():
     return render_template("outside/index.html")
+
+
+@bp.route("/_health", methods=["GET"])
+def _health():
+    return "OK"
