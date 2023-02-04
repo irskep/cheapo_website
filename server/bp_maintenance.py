47ce829aa924d89f6333082aecb0f145297dc774
Steve Landey <steve@steveasleep.com>
2023-02-04 12:46:08 -0800

    Maintenance mode is a catchall route

--- server/bp_maintenance.py
+++ server/bp_maintenance.py
@@ -3,12 +3,13 @@ from flask import Blueprint, render_template
 bp = Blueprint("maintenance", "maintenance", url_prefix="")
 
 
-@bp.route("/", methods=["GET"])
-def index():
-    return render_template("maintenance/index.html")
-
-
 @bp.route("/_health", methods=["GET"])
 def _health():
     # This route is used during deployment to check that the server process is running.
     return "OK"
+
+
+@bp.route("/", methods=["GET"], defaults={"path": ""})
+@bp.route("/<path:path>", methods=["GET"])
+def index():
+    return render_template("maintenance/index.html")
